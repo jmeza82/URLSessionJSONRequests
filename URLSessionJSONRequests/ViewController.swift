41573c56d3321625a593c284e9865e24c3e5e1fc
Juan Meza <juanmeza@Juans-MacBook-Pro.local>
2018-01-10 19:40:01 -0800

    Example URLSessionJSONRequest

--- URLSessionJSONRequests/ViewController.swift
+++ URLSessionJSONRequests/ViewController.swift
@@ -15,11 +15,69 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onGetTapped(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {
+            return
+            
+        }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) {(data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+        }.resume()
+        
     }
+    
 
-
+    @IBAction func onPostTapped(_ sender: Any) {
+        
+        let parameters = ["username":"@kilo_loco","tweet":"helloworld"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            return
+        }
+        var request = URLRequest(url:url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            return
+        }
+        
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: request) {(data, response, error) in
+            
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+        }.resume()
+    }
+    
 }
 
